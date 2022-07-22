4d4250eb006311d1ed6b98a9ca3ac46b407fce58
Denis Maina <Denis.maina@student.moringaschool.com>
2022-07-22 16:00:20 +0300

    initial commit

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,10 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { makeRandomNumber } from "../utils";
 
 function Ticker() {
   const [price, setPrice] = useState(0);
   const [color, setColor] = useState("black");
+  //create the ref and set its initial value
+  const prevPriceRef = useRef(price);
 
+  useEffect(() => {
+    // we need some way to get the prevPrice... here we use the current value of ref
+    const prevPrice = prevPriceRef.current;
+    if (price > prevPrice) {
+      setColor("green");
+    } else if (price < prevPrice) {
+      setColor("red");
+    } else {
+      setColor("black");
+    }
+    //set a new value of ref to compare it with the old one  
+    prevPriceRef.current = price;
+  }, [price]);
   useEffect(() => {
     const id = setInterval(() => setPrice(makeRandomNumber), 1000);
     return function () {
